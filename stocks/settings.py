9d775cd414405ce13e05c6d1a9f591f7e569daad
akshatpapnoi <akshatpapnoi1110@gmail.com>
2020-05-21 02:50:47 +0530

    wsgi file updated

--- stocks/settings.py
+++ stocks/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '+!ob)v62ev3+u#_x-%6vzg9l-0fbbxc&kov)*!h41e*uup5az_'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
